d929b464192b545fae46b449bfe7267ab53a222b
Alisson2807 <alissonsilva280708@gmail.com>
2025-04-25 16:54:19 -0300

    Add files via upload

---
+++ relacional.py
@@ -0,0 +1,4 @@
+a = 10 
+b = 5
+resutado = a > b 
+print(resutado) # true
---
+++ repeticao.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(f"Iteração {i}")
+
+print(______________________________________________________)
+
+contador = 0
+while contador < 5:
+    contador += 1
+    print(f"contador: {contador}")
+
+    print(___________________________________________________)
+
+    for i in range(10):
+        if i == 5:
+            break
+        print(i)
